e822c62c597ccd835363513ee5be554c52ab7db8
Herik Colares Rezende <dev.herik@gmail.com>
2025-05-30 04:53:30 -0300

    logic: login working

--- src/app/pages/dashboard/layout.tsx
+++ src/app/pages/dashboard/layout.tsx
@@ -5,17 +5,15 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <nav>
-          Nav Bar
-          <button onClick={logout} className={`m-1`} type="button">
-            Logout
-          </button>
-        </nav>
-        <main>{children}</main>
-        <footer>Footer</footer>
-      </body>
-    </html>
+    <>
+      <nav>
+        Nav Bar
+        <button onClick={logout} className={`m-1`} type="button">
+          Logout
+        </button>
+      </nav>
+      <main>{children}</main>
+      <footer>Footer</footer>
+    </>
   );
 }
--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -4,7 +4,7 @@ import { login } from "@/app/posts/actions";
 export default function Login() {
   return (
     <div className={`flex p-6 justify-center items-center h-screen`}>
-      <div className="bg-blue-800 p-6 rounded-2xl">
+      <div className="bg-blue-800 p-6 rounded-2xl shadow-md">
         <Form
           className={`flex flex-col gap-2 justify-around p-4`}
           action={login}
@@ -23,8 +23,8 @@ export default function Login() {
             placeholder="Password"
             required
           />
-          <button className={`m-1`} type="submit">
-            Login
+          <button className={`m-1 text-[#1e40af] bg-[#fffcff] p-2.5 rounded-md shadow-md`} type="submit">
+            Sign In
           </button>
         </Form>
       </div>
